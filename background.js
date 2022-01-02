af7a323d6a88e2402a9f6f98639b724f32f68b1a
PressJump <40802367+PressJump@users.noreply.github.com>
2022-01-02 19:00:22 +1100

    🚑️ Background Script Fix
    Co-Authored-By: trickypr <23250792+TrickyPr@users.noreply.github.com>

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
 //@ts-check
 /// <reference types="web-ext-types"/>
 
+console.log('Emoji Picker hello world')
 
+console.log(
+  browser.menus.create({
+    title: 'Emoji Picker',
+  })
+)
 
-browser.contextMenus.create({
-    id: "emoji-picker",
-    title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
-    contexts: ["selection"]
-  }, onCreated);
-  
-
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-    switch (info.menuItemId) {
-        case "log-selection":
-        console.log(info.selectionText);
-        break;
-    }
+browser.menus.onClicked.addListener(function (info, tab) {
+  switch (info.menuItemId) {
+    case 'log-selection':
+      console.log(info.selectionText)
+      break
+  }
 })
